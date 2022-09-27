51ddc62dc152db2e87d798b592449925dbb43b26
Marcus Virginia <virgmar@chrobinson.com>
2022-09-27 13:47:38 -0500

    fix docusaurus build issues and add react context

--- packages/convenience-store/docs/code/intro.md
+++ packages/convenience-store/docs/code/intro.md
@@ -1,5 +1,9 @@
+---
+sidebar_position: 1
+---
+
 # Intro
 
-This isle of the store contains code snippets and
+This isle of the store contains code snippets and small explanations of language/framework specific features
 
 > This isle is under construction
--- packages/convenience-store/docusaurus.config.js
+++ packages/convenience-store/docusaurus.config.js
@@ -1,8 +1,7 @@
 // @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
 
 const lightCodeTheme = require('prism-react-renderer/themes/github')
-const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+const darkCodeTheme = require('prism-react-renderer/themes/vsDark')
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -18,129 +17,117 @@ const config = {
 
   presets: [
     [
-      'classic',
+      '@docusaurus/preset-classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            'https://github.com/MarcusVirg/convenience-store/tree/main/docs/'
+            'https://github.com/MarcusVirg/web/tree/main/packages/convenience-store/docs'
         },
-        // blog: {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   editUrl:
-        //     "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        // },
         theme: {
-          customCss: require.resolve('./src/css/custom.css')
+          customCss: [require.resolve('./src/css/custom.css')]
         }
-      })
+      }
     ]
   ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: "Marcus's Convenience Store",
-        logo: {
-          alt: 'Convenience Store',
-          src: 'img/logo.svg'
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  themeConfig: {
+    navbar: {
+      title: "Marcus's Convenience Store",
+      logo: {
+        alt: 'Convenience Store',
+        src: 'img/logo.svg'
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'code/intro',
+          position: 'left',
+          label: 'Code'
         },
-        items: [
-          {
-            type: 'doc',
-            docId: 'code/intro',
-            position: 'left',
-            label: 'Code'
-          },
-          {
-            type: 'doc',
-            docId: 'patterns/intro',
-            position: 'left',
-            label: 'Patterns'
-          },
-          {
-            type: 'doc',
-            docId: 'process/intro',
-            position: 'left',
-            label: 'Process'
-          },
-          // { to: "/blog", label: "Blog", position: "left" },
-          {
-            href: 'https://github.com/MarcusVirg',
-            label: 'GitHub',
-            position: 'right'
-          }
-        ]
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Isles',
-            items: [
-              {
-                label: 'Code',
-                to: '/docs/code/intro'
-              },
-              {
-                label: 'Patterns',
-                to: '/docs/patterns/intro'
-              },
-              {
-                label: 'Process',
-                to: '/docs/process/intro'
-              }
-            ]
-          },
-          {
-            title: 'Community',
-            items: [
-              // {
-              //   label: "Stack Overflow",
-              //   href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              // },
-              // {
-              //   label: "Discord",
-              //   href: "https://discordapp.com/invite/docusaurus",
-              // },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/MarcusVirg'
-              }
-            ]
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Portfolio',
-                to: 'https://marcusv.me'
-              },
-              {
-                label: 'Blog',
-                to: 'https://marcusv.me/blog'
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/MarcusVirg'
-              }
-            ]
-          }
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} MarcusVirg. Built with Docusaurus.`
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme
-      },
-      colorMode: {
-        defaultMode: 'dark',
-        respectPrefersColorScheme: true
-      }
-    })
+        {
+          type: 'doc',
+          docId: 'patterns/intro',
+          position: 'left',
+          label: 'Patterns'
+        },
+        {
+          type: 'doc',
+          docId: 'process/intro',
+          position: 'left',
+          label: 'Process'
+        },
+        {
+          href: 'https://github.com/MarcusVirg',
+          label: 'GitHub',
+          position: 'right'
+        }
+      ]
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Isles',
+          items: [
+            {
+              label: 'Code',
+              to: '/docs/code/intro'
+            },
+            {
+              label: 'Patterns',
+              to: '/docs/patterns/intro'
+            },
+            {
+              label: 'Process',
+              to: '/docs/process/intro'
+            }
+          ]
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/MarcusVirg'
+            }
+          ]
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Portfolio',
+              to: 'https://marcusv.me'
+            },
+            {
+              label: 'Blog',
+              to: 'https://marcusv.me/blog'
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/MarcusVirg'
+            }
+          ]
+        }
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} MarcusVirg. Built with Docusaurus.`
+    },
+    prism: {
+      /** @type {import("prism-react-renderer").PrismTheme} */
+      // @ts-ignore
+      theme: lightCodeTheme,
+      /** @type {import("prism-react-renderer").PrismTheme} */
+      // @ts-ignore
+      darkTheme: darkCodeTheme
+    },
+    colorMode: {
+      defaultMode: 'dark',
+      respectPrefersColorScheme: true
+    }
+  }
 }
 
 module.exports = config
