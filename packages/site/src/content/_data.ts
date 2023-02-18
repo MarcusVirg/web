88f2af13bd237e719dd51625e264182468641ba4
Marcus Virginia <marcusvirg345@gmail.com>
2023-02-18 15:32:09 -0600

    finish about page

---
+++ packages/site/src/content/_data.ts
@@ -0,0 +1,22 @@
+export const socials = [
+	{
+		name: 'mastodon',
+		icon: 'logo-mastodon',
+		url: 'https://mastodon.social/@marcusvirginia'
+	},
+	{
+		name: 'instagram',
+		icon: 'logo-instagram',
+		url: 'https://www.instagram.com/marcusjvirginia/'
+	},
+	{
+		name: 'github',
+		icon: 'logo-github',
+		url: 'https://github.com/MarcusVirg'
+	},
+	{
+		name: 'linkedin',
+		icon: 'logo-linkedin',
+		url: 'https://www.linkedin.com/in/marcusvirginia/'
+	}
+]
